29b07291e6921e1c77d1bc8c96aa56da3586cc89
Ramin Radmand <ramin.radmand@gmail.com>
2021-01-26 20:36:08 -0700

    setup routes

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const url = require('url')
 
 
 const server = http.createServer((req, res) => {
-    res.end('test')
+    const pathName = req.url
+
+    if (pathName === '/overview' || pathName === '/') {
+        res.end('overview')
+    } else if (pathName === '/product') {
+        res.end('Product')
+    } else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html'
+        })
+        res.end('<h1>Page not found<h1>')
+    }
+
 })
 
 server.listen(8080, () => {
