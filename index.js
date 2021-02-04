d83a09cc541601ada3469b2d0d5f91c2969dc8e7
Ramin Radmand <ramin.radmand@gmail.com>
2021-02-03 19:55:20 -0700

    made function to replace placeholders with actual

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const http = require('http')
 const url = require('url')
 
 
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%product_name%}/g, product.productName)
+    output = output.replace(/{%location%}/g, product.from)
+    output = output.replace(/{%emoji%}/g, product.image)
+    output = output.replace(/{%ingridents%}/g, product.nutrients)
+    output = output.replace(/{%price%}/g, product.price)
+    output = output.replace(/{%description%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    output = output.replace(/{%quantity%}/g, product.quantity)
+
+    if(!product.organic) {
+        output = output.replace(/{%not_organic%}/g, 'not-organic')
+    }
+    return output
+}
+// replaces empty card with actual products
+
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8')
 const overviewTemplate = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
 const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
@@ -18,7 +35,11 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             "Content-type": "text/html"
         })
-        res.end(overviewTemplate)
+
+        const overviewCards = dataObj.map(el => replaceTemplate(cardTemplate, el)).join('')
+        const actualOverviewPage = overviewTemplate.replace('{%card%}', overviewCards)
+
+        res.end(actualOverviewPage)
 
     //products page
     } else if (pathName === '/product') {
